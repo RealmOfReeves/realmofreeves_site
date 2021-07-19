0e8dec560160b8d59ffa9aae9739901be076cb3a
RealmOfReeves <blobbishtheartist@gmail.com>
2021-07-18 21:16:54 -0500

    Started CSS styling and stuff

---
+++ assets/js/footer.js
@@ -0,0 +1,7 @@
+document.write(`
+    <footer align="center">
+        <p>Comics made by Reevet.</p>
+        <p><stromg>Since 07/16/2021</strong></p>
+        <p>Hosted on github pages </p>
+    </footer>
+`);
---
+++ assets/js/header.js
@@ -0,0 +1,10 @@
+document.write(`
+    <header align="center">
+        <a href="index.html"><img src="assets/img/header_logo.png" alt="Realm Of Reeves Logo" /></a>
+        <div id="nav">
+            <a href="index.html">HOME</a> |
+            <a href="about.html">ABOUT</a> 
+        </div>
+    </header>
+
+`)
